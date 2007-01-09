f07e2376d91a9fa2da32533e9cad32548bd0fbe8
Eric Everman <eeverman@usgs.gov>
2007-01-09 23:31:10 +0000

    -New Data2D class that compares to Data2D instances

---
+++ src/gov/usgswim/sparrow/Data2DCompare.java
@@ -0,0 +1,93 @@
+package gov.usgswim.sparrow;
+
+/**
+ * A Data2D implementation that derives its from a comparison between two
+ * Data2D instances.
+ * 
+ * Comparisons are always in terms of the baseData - compData.
+ */
+public class Data2DCompare extends Data2DView {
+	Data2D compData;
+	int[] colMap;
+	private Double[] maxCompValues;  //Max deviation for each column.  Each value may be null if unknown
+	
+	/**
+	 * Construct a new instance using the passed baseData and compData.
+	 * The values returned from this instance at any row column are always
+	 * baseData - compData.
+	 * 
+	 * @param baseData
+	 * @param compData
+	 */
+	public Data2DCompare(Data2D baseData, Data2D compData) {
+		super(baseData, 0, baseData.getColCount());
+		
+		this.compData = compData;
+	}
+	
+	/**
+	 * Construct a new instance using the passed baseData and compData.
+	 * The values returned from this instance at any row column are always
+	 * baseData - compData.
+	 * 
+	 * The column mapping specifies how the column in baseData map to compData.
+	 * For instance, if column index 4 of baseData should be compared to column
+	 * index 9 of compData, index 4 of the columnMapping should contain a 9.
+	 * 
+	 * The passed columnMapping array size must match the number of columns in
+	 * baseData.  There is no such requirement to match columnMapping (ie, the
+	 * compData may contain un-needed columns).
+	 * 
+	 * @param baseData
+	 * @param compData
+	 * @param columnMapping
+	 */
+	public Data2DCompare(Data2D baseData, Data2D compData, int[] columnMapping) {
+		super(baseData, 0, baseData.getColCount());
+		
+		this.compData = compData;
+		colMap = columnMapping;
+		maxCompValues = new Double[baseData.getColCount()];
+	}
+	
+	public double compare(int row, int col) throws IndexOutOfBoundsException {
+		return super.getDouble(row, col) - compData.getDouble(row, mapColumn(col));
+	}
+	
+	public synchronized double findMaxCompareValue(int column) {
+		if (maxCompValues[column] == null) {
+
+			double max = Double.MIN_VALUE;
+			
+			for (int r = 0; r < getRowCount(); r++)  {
+				double d = Math.abs( compare(r, column) );
+				if (d > max) max = d;
+			}
+			
+			maxCompValues[column] = new Double(max);
+			
+		}
+		return maxCompValues[column].doubleValue();
+	}
+	
+	public synchronized double findMaxCompareValue() {
+
+		double max = Double.MIN_VALUE;
+		
+		for (int i = 0; i < maxCompValues.length; i++)  {
+			double d = findMaxCompareValue(i);
+			if (d > max) max = d;
+		}
+
+		return max;
+	}
+	
+	protected int mapColumn(int c) {
+		if (colMap != null) {
+			return colMap[c];
+		} else {
+			return c;
+		}
+	}
+
+}
