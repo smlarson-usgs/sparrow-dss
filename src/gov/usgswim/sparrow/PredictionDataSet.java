b732b133c1e413f558c7f4913de6939f6e475ffc
Eric Everman <eeverman@usgs.gov>
2007-03-28 21:50:23 +0000

    -New class to bundle all data needed for a prediction

---
+++ src/gov/usgswim/sparrow/PredictionDataSet.java
@@ -0,0 +1,259 @@
+package gov.usgswim.sparrow;
+
+/**
+ * Databean that packages all of the data required to run a prediction.
+ * 
+ * This class is not thread safe!  Once created, this class may be cached and
+ * used for prediction runs, so do not reassign or change the values it contains!
+ */
+public class PredictionDataSet {
+
+	/**
+	 * One row per reach (i = reach index)
+	 * <ol>
+	 * <li>[i][0] REACH_ID - The system id for the reach (db unique id)
+	 * <li>[i][1] HYDSEQ - The model specific hydrological sequence number
+	 * </ol>
+	 */
+	protected Data2D sys;
+	
+	/**
+	 * Invariant topographic info about each reach
+	 * i = reach index
+	 * [i][0]	from node index
+	 * [i][1]	too node index
+	 * [i][2]	'if transmit' is 1 if the reach transmits to its too-node
+	 * 
+	 * NOTE:  We assume that the node indexes start at zero and have no skips.
+	 * Thus, nodeCount must equal the largest node index + 1
+	 */
+	protected Data2D topo;
+	
+	/**
+	 * The coef's for each reach-source.
+	 * coef[i][k] == the coefficient for source k at reach i
+	 */
+	protected Data2D coef;
+	
+	/**
+	 * The source amount for each reach-source.
+	 * src[i][k] == the amount added via source k at reach i
+	 */
+	protected Data2D src;
+	
+	/**
+	 * The stream and resevor decay.  The values in the array are *actually* 
+	 * delivery, which is (1 - decay).  I.E. the delivery calculation is already
+	 * done.
+	 * 
+	 * src[i][0] == the instream decay at reach i.
+	 *   This decay is assumed to be at mid-reach and already computed as such.
+	 *   That is, it would normally be the sqr root of the instream decay, and
+	 *   it is assumed that this value already has the square root taken.
+	 * src[i][1] == the upstream decay at reach i.
+	 *   This decay is applied to the load coming from the upstream node.
+	 */
+	protected Data2D decay;
+	
+	public PredictionDataSet() {
+	}
+	
+	/**
+	 * Constructs a new dataset w/ all data tables defined.
+	 * See matching method docs for complete definitions of each parameter.
+	 * 
+	 * @param topo
+	 * @param coef
+	 * @param src
+	 * @param decay
+	 * @param sys
+	 */
+	public PredictionDataSet(Data2D topo, Data2D coef, Data2D src, Data2D decay, Data2D sys) {
+		this.topo = topo;
+		this.coef = coef;
+		this.src = src;
+		this.decay = decay;
+		this.sys = sys;
+	}
+	
+	/**
+	 * Constructs a new dataset w/o system info, which means it will not be possible
+	 * to match the result data to other data (no unique id).
+	 * 
+	 * See matching method docs for complete definitions of each parameter.
+	 * 
+	 * @param topo
+	 * @param coef
+	 * @param src
+	 * @param decay
+	 * @param sys
+	 */
+	public PredictionDataSet(Data2D topo, Data2D coef, Data2D src, Data2D decay) {
+		this.topo = topo;
+		this.coef = coef;
+		this.src = src;
+		this.decay = decay;
+	}
+	
+
+	/**
+	 * Assigns the topo data.
+	 * 
+	 * <h4>Data Columns, sorted by HYDSEQ.  One row per reach (i = reach index)</h4>
+	 * <ol>
+	 * <li>[i][0] FNODE - The from node
+	 * <li>[i][1] TNODE - The to node
+	 * <li>[i][2] IFTRAN - 1 if this reach transmits to its end node, 0 otherwise
+	 * </ol>
+	 * 
+	 * NOTE:  Node indexes that do not start at zero or have large skips will
+	 * require more memory to process.
+	 * @param topo
+	 */
+	public void setTopo(Data2D topo) {
+		this.topo = topo;
+	}
+
+	/**
+	 * Returns the topo data.
+	 * 
+	 * <h4>Data Columns, sorted by HYDSEQ.</h4>
+	 * <p>One row per reach (i = reach index)</p>
+	 * <ol>
+	 * <li>[i][0] FNODE - The from node
+	 * <li>[i][1] TNODE - The to node
+	 * <li>[i][2] IFTRAN - 1 if this reach transmits to its end node, 0 otherwise
+	 * <li>[i][3] (optional - this column and others ignored) HYDSEQ - Sort identifier such that any lower number reach is upstream of a higher numbered one.
+	 * </ol>
+	 * 
+	 * NOTE:  Node indexes that do not start at zero or have large skips will
+	 * require more memory to process.
+	 * @return The Data2D data
+	 */
+	public Data2D getTopo() {
+		return topo;
+	}
+
+	/**
+	 * Set the Coef data
+	 *
+	 * <h4>Data Columns, sorted by HYDSEQ</h4>
+	 * <p>One row per reach (i = reach index).  coef[i][k] == the coefficient for source k at reach i</p>
+	 * @param coef
+	 */
+	public void setCoef(Data2D coef) {
+		this.coef = coef;
+	}
+
+	/**
+	 * Returns the coef data.
+	 * 
+	 * <h4>Data Columns, sorted by HYDSEQ</h4>
+	 * <p>One row per reach (i = reach index).  coef[i][k] == the coefficient for source k at reach i</p>
+	 * 
+	 * @return The Data2D data
+	 */
+	public Data2D getCoef() {
+		return coef;
+	}
+	 
+	 
+	/**
+	 * Sets the source data
+	 * 
+	 * <h4>Data Columns, sorted by HYDSEQ</h4>
+	 * <p>One row per reach (i = reach index).  coef[i][k] == the source value for source k at reach i</p>
+	 * @param src
+	 */
+	public void setSrc(Data2D src) {
+		this.src = src;
+	}
+
+	/**
+	 * Returns the source data
+	 * 
+	 * <h4>Data Columns, sorted by HYDSEQ</h4>
+	 * <p>One row per reach (i = reach index).  coef[i][k] == the source value for source k at reach i</p>
+	 * 
+	 * @return The Data2D data
+	 */
+	public Data2D getSrc() {
+		return src;
+	}
+
+	/**
+	 * Assigns the decay values.
+	 * 
+	 * <h4>Data Columns, sorted by HYDSEQ</h4>
+	 * <p>One row per reach (i = reach index)</p>
+	 * <ol>
+	 * <li>[i][0] == the instream decay at reach i.<br>
+	 *   This decay is assumed to be at mid-reach and already computed as such.
+	 *   That is, it would normally be the sqr root of the instream decay, and
+	 *   it is assumed that this value already has the square root taken.
+	 * <li>src[i][1] == the upstream decay at reach i.<br>
+	 *   This decay is applied to the load coming from the upstream node.
+	 * <li>Additional columns ignored
+	 * </ol>
+	 * 
+	 * @param decay
+	 */
+	public void setDecay(Data2D decay) {
+		this.decay = decay;
+	}
+
+	/**
+	 * Returns the decay data.
+	 * 
+	 * <h4>Data Columns, sorted by HYDSEQ</h4>
+	 * <p>One row per reach (i = reach index)</p>
+	 * <ol>
+	 * <li>[i][0] == the instream decay at reach i.<br>
+	 *   This decay is assumed to be at mid-reach and already computed as such.
+	 *   That is, it would normally be the sqr root of the instream decay, and
+	 *   it is assumed that this value already has the square root taken.
+	 * <li>src[i][1] == the upstream decay at reach i.<br>
+	 *   This decay is applied to the load coming from the upstream node.
+	 * <li>Additional columns ignored
+	 * </ol>
+	 * 
+	 * @return
+	 */
+	public Data2D getDecay() {
+		return decay;
+	}
+
+	/**
+	 * Assigns the system information, which is optional but required if the
+	 * results are to be correlated to other data in the db.
+	 * 
+	 * <h4>Data Columns, sorted by HYDSEQ</h4>
+	 * <p>One row per reach (i = reach index)</p>
+	 * <ol>
+	 * <li>[i][0] REACH_ID - The system id for the reach (db unique id)
+	 * <li>[i][1] HYDSEQ - The model specific hydrological sequence number
+	 * </ol>
+	 * 
+	 * @param sys
+	 */
+	public void setSys(Data2D sys) {
+		this.sys = sys;
+	}
+
+	/**
+	 * Returns the system information, which is optional but required if the
+	 * results are to be correlated to other data in the db.
+	 * 
+	 * <h4>Data Columns, sorted by HYDSEQ</h4>
+	 * <p>One row per reach (i = reach index)</p>
+	 * <ol>
+	 * <li>[i][0] REACH_ID - The system id for the reach (db unique id)
+	 * <li>[i][1] HYDSEQ - The model specific hydrological sequence number
+	 * </ol>
+	 * 
+	 * @return
+	 */
+	public Data2D getSys() {
+		return sys;
+	}
+}
