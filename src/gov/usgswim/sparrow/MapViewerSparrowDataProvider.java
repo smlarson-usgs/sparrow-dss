e08db05b3fc036ec95dfaf5942304e360515cd92
Eric Everman <eeverman@usgs.gov>
2007-06-25 03:34:20 +0000

    -Now uses the new cache

--- src/gov/usgswim/sparrow/MapViewerSparrowDataProvider.java
+++ src/gov/usgswim/sparrow/MapViewerSparrowDataProvider.java
@@ -1,5 +1,6 @@
 package gov.usgswim.sparrow;
 
+import gov.usgswim.sparrow.service.SharedApplication;
 import gov.usgswim.sparrow.util.JDBCUtil;
 
 import java.sql.Connection;
@@ -157,9 +158,9 @@ public class MapViewerSparrowDataProvider implements NSDataProvider {
 	 * @return
 	 */
 	public NSDataSet buildDataSet(Hashtable properties) {
-		int modelId = Integer.parseInt( properties.get(MODEL_ID_KEY).toString() );
-		boolean ignoreCache = Boolean.parseBoolean( (String) properties.get(IGNORE_CACHE_KEY) );
-		String globalSrcAdj = (String) properties.get(GROSS_SOURCE_ADJUST_KEY);
+		Data2D result = null;		//The prediction result
+		
+		long modelId = Long.parseLong( properties.get(MODEL_ID_KEY).toString() );
 		String resultMode = (String) properties.get(RESULT_MODE_KEY);
 		
 		boolean isPercCompareMode = StringUtils.equalsIgnoreCase(RESULT_MODE_PERC_CHG, resultMode);
@@ -168,250 +169,92 @@ public class MapViewerSparrowDataProvider implements NSDataProvider {
 		boolean isValueMode = ! (isPercCompareMode || isDecPercCompareMode);	//The default
 		boolean isCompareMode = ! isValueMode;		//Convience flag
 		
-		
-		NSDataSet data = null;
+		NSDataSet nsData = null;
 		
 		long startTime = System.currentTimeMillis();	//Time started
-		long incStartTime = 0;	//Start time at each incremental step
-		long incEndTime = 0;	//end time at each incremental step
-		
-		log.info("MVSparrowDataProvider buildDataSet: " + "model_id" + " = " + modelId);
-		
+
 		try {
+			AdjustmentSetBuilder adjBuilder = new AdjustmentSetBuilder();
+			adjBuilder.setAdjustments(properties);
+			PredictionRequest adjRequest = new PredictionRequest(modelId, adjBuilder.getImmutable());
+			Data2D adjResult = SharedApplication.getInstance().getPredictResultCache().compute(adjRequest);
 			
-			Data2D result = null;		//The prediction result
-			Data2D sysInfo = null;	//The system info associated w/ the result (contains the unique id numbers)
-			PredictionDataSet nonAdjustedData = null;	//The dataset w/ no source adjustments
-			PredictionDataSet adjustedData = null;	//The dataset after source adjustments
-			
-			//Load the data from the db (or cache)
-			incStartTime = System.currentTimeMillis();
-			nonAdjustedData = loadData(modelId, ignoreCache);
-			
-			incEndTime = System.currentTimeMillis();
-			log.debug("MVSparrowDataProvider data loaded (" + nonAdjustedData.getSrc().getRowCount() + " rows)  Time Spent: " + (incEndTime - incStartTime) + "ms");
-			
-			//Do source adjustments
-			incStartTime = System.currentTimeMillis();
-			adjustedData = adjustSources(nonAdjustedData, globalSrcAdj);
-			incEndTime = System.currentTimeMillis();
-			log.debug("MVSparrowDataProvider sources adjusted.  Time Spent: " + (incEndTime - incStartTime) + "ms");
-			
-			//Run the prediction (adjusted source values)
-			incStartTime = System.currentTimeMillis();
-			PredictSimple adjPredict = new PredictSimple(adjustedData);
-			result = adjPredict.doPredict();
-			adjPredict = null;
-			sysInfo = adjustedData.getSys();
-			incEndTime = System.currentTimeMillis();
-			log.debug("MVSparrowDataProvider adjusted predict complete.  Time Spent: " + (incEndTime - incStartTime) + "ms");
-			
-			//Run the prediction on the non adjusted value (if doing comparison)
 			if (isCompareMode) {
-				incStartTime = System.currentTimeMillis();
-				PredictSimple nonAdjPredict = new PredictSimple(nonAdjustedData);
-				Double2D nonAdjResult = null;
-				nonAdjResult = nonAdjPredict.doPredict();
-				nonAdjPredict = null;
-				result = new Data2DPercentCompare(nonAdjResult, result, isDecPercCompareMode, true);
-				incEndTime = System.currentTimeMillis();
-				log.debug("MVSparrowDataProvider non-adjusted predict complete.  Time Spent: " + (incEndTime - incStartTime) + "ms");	
-			}
-			
-			//Copy the result data to an NSDataSet
-			incStartTime = System.currentTimeMillis();
-			int rowCount = result.getRowCount();
-			int colCount = result.getColCount();
-			NSRow[] nsRows = new NSRow[rowCount];
-			
-			for (int r=0; r < rowCount; r++) {
-				Field[] row = new Field[2];	//ID
-				row[0] = new Field(sysInfo.getInt(r, 0));
-				row[0].setKey(true);
-				
-				row[1] = new Field(result.getDouble(r, colCount - 1));	//Value
-				//row[1].setLabelText(true);
-
-				NSRow nsRow = new NSRow(row);
-				nsRows[r] = nsRow;
-			}
-			
-			data = new NSDataSet(nsRows);
-			
-			incEndTime = System.currentTimeMillis();
-			log.debug("MVSparrowDataProvider transfer to NSDataSet complete.  Time Spent: " + (incEndTime - incStartTime) + "ms");
-			log.info("MVSparrowDataProvider Returning data (" + rowCount + " rows).  Total time spent: " + (System.currentTimeMillis() - startTime) + "ms");
-			
-			
-			
-			//Debug top 10 values
-			if (log.isDebugEnabled()) {
-				int maxRow = 10;
-				if (maxRow > nsRows.length) maxRow = nsRows.length;
-				
-				log.debug("MVSparrowDataProvider These are the first ten rows of data: ");
-				for (int r = 0; r < maxRow; r++)  {
-					StringBuffer sb = new StringBuffer();
-					for (int c = 0; c < nsRows[0].size(); c++)  {
-						sb.append(nsRows[r].get(c).toString());
-						if (nsRows[r].get(c).isKey()) sb.append("[Key] ");
-						if (nsRows[r].get(c).isLabelText()) sb.append("[Lab] ");
-						if ((c + 1) < nsRows[0].size()) sb.append("| ");
-					}
-					log.debug(sb.toString());
-				}
-				
-			}
-			
-			
-		} catch (Exception e) {
-			log.error("No way to indicate this error to mapViewer...", e);
-			return null;
-		}
-		
-		return data;
-	}
+				//need to run the base prediction and the adjusted prediction
+				AdjustmentSetImm noAdj = new AdjustmentSetImm();
+				PredictionRequest noAdjRequest = new PredictionRequest(modelId, noAdj);
+				Data2D noAdjResult = SharedApplication.getInstance().getPredictResultCache().compute(noAdjRequest);
 	
-	/**
-	 * This method is not syncrhonized, but the risk is basically that the same
-	 * data will be loaded twice.
-	 * @param modelId
-	 * @return
-	 * @throws NamingException
-	 * @throws SQLException
-	 * @throws ClassNotFoundException
-	 * @throws Exception
-	 */
-	protected PredictionDataSet loadData(int modelId, boolean ignoreCache) throws NamingException, SQLException,
-			ClassNotFoundException, Exception {
-			
-		Integer key = new Integer(modelId);
-		Object data = null;	
-		
-		if (! ignoreCache) {
-			data = cachedData.get(key);
-			
-			if (data != null) {
-				log.debug("MVSparrowDataProvider found cached data for model_id " + modelId);
-				data = ((DataProviderCacheProxy) data).getData();
-				
-				if (data != null) {
-					log.debug("MVSparrowDataProvider cache getData() returned non-null data for model_id " + modelId);
-					return (PredictionDataSet) data;
-				} else {
-					log.warn("MVSparrowDataProvider cache getData() returned NULL data for model_id " + modelId);
-				}
-				
-				
+				result = new Data2DPercentCompare(noAdjResult, adjResult, isDecPercCompareMode, true);
+			} else {
+				//need to run only the adjusted prediction
+				result = adjResult;
 			}
+			
+			Data2D sysInfo = SharedApplication.getInstance().getPredictDatasetCache().compute( modelId ).getSys();
+			nsData = copyToNSDataSet(result, sysInfo);
+			
+			log.debug("MVSparrowDataProvider done for model #" + modelId + " (" + nsData.size() + " rows) Time: " + (System.currentTimeMillis() - startTime) + "ms");
+			
+			return nsData;
+			
+		} catch (InterruptedException e) {
+			log.error("No way to indicate this error to mapViewer, so throwing a runtime exception.", e.getCause());
+			throw new RuntimeException(e);
 		}
-
-
-		//Couldn't return a cached dataset
-		{
 		
-			//Create a proxy placehold to prevent others from trying to create
-			DataProviderCacheProxy proxy = new DataProviderCacheProxy(key);
-			cachedData.put(key, proxy);
-			
-			Connection conn = null;
-			try {
-			
-				conn = getConnection();
-				data = JDBCUtil.loadMinimalPredictDataSet(conn, modelId);
-				proxy.setData(data);
-				return (PredictionDataSet) data;
-				
-			} finally {
-				if (conn != null) {
-					try {
-						conn.close();
-					} catch (Exception ee) {
-						/* Ignore */
-					}
-					conn = null;
-				}
-			}
-		}
 
 	}
 	
-	/**
-	 * @todo This assumes the id number is the same as the column position.
-	 * @param data
-	 * @param globalSrcAdj
-	 */
-	protected PredictionDataSet adjustSources(PredictionDataSet data, String globalSrcAdj) {
-		globalSrcAdj = StringUtils.trimToNull(globalSrcAdj);
+	protected NSDataSet copyToNSDataSet(Data2D result, Data2D sysInfo) {
+
+		int rowCount = result.getRowCount();
+		int colCount = result.getColCount();
+		NSRow[] nsRows = new NSRow[rowCount];
 		
-		log.debug("MVSparrowDataProvider adjustment STring: " + globalSrcAdj);
-		
-		if (globalSrcAdj != null) {
-			String[] sVals = StringUtils.split(globalSrcAdj, ", :;|");
-			double[] dVals = new double[sVals.length];
+		for (int r=0; r < rowCount; r++) {
+			Field[] row = new Field[2];	//ID
+			row[0] = new Field(sysInfo.getInt(r, 0));
+			row[0].setKey(true);
 			
-			for (int i = 0; i < dVals.length; i++)  {
-				dVals[i] = Double.parseDouble(sVals[i]);
-			}
-			
-			Data2DColumnCoefView view = new Data2DColumnCoefView(data.getSrc());
-			
-			for (int i = 0; i < dVals.length; i+=2)  {
-				int col = ((int) dVals[i]) - 1;
-				double coef = dVals[i + 1];
-				view.setCoef(col, coef);
-			}
-			
-			return new PredictionDataSet(data.getTopo(), data.getCoef(), view, data.getDecay(), data.getSys());
-			
-		} else {
-			
-			return data;	//no adjustment
+			row[1] = new Field(result.getDouble(r, colCount - 1));	//Value
+			//row[1].setLabelText(true);
+	
+			NSRow nsRow = new NSRow(row);
+			nsRows[r] = nsRow;
 		}
 		
+		if (log.isDebugEnabled()) {
+			debugNSData(nsRows);
+		}
 		
+		return new NSDataSet(nsRows);
+	}
+	
+	protected void debugNSData(NSRow[] nsRows) {
+		int maxRow = 10;
+		if (maxRow > nsRows.length) maxRow = nsRows.length;
+		
+		log.debug("MVSparrowDataProvider These are the first ten rows of data: ");
+		for (int r = 0; r < maxRow; r++)  {
+			StringBuffer sb = new StringBuffer();
+			for (int c = 0; c < nsRows[0].size(); c++)  {
+				sb.append(nsRows[r].get(c).toString());
+				if (nsRows[r].get(c).isKey()) sb.append("[Key] ");
+				if (nsRows[r].get(c).isLabelText()) sb.append("[Lab] ");
+				if ((c + 1) < nsRows[0].size()) sb.append("| ");
+			}
+			log.debug(sb.toString());
+		}
 	}
 
 	/**
 	 * Called once when this instance is destroyed
 	 */
 	public void destroy() {
-		jndiDS = null;
+		//
 	}
 	
-	public synchronized Connection getConnection() throws NamingException, SQLException,
-																												ClassNotFoundException {
 
-		Connection conn = null;
-
-		if (jndiDatasourceName != null) {
-			if (jndiDS == null) {
-				log.debug("Build new data jdbc datasouce from jndi name '" + jndiDatasourceName + "'");
-				InitialContext context = new InitialContext();
-				jndiDS = (DataSource)context.lookup(jndiDatasourceName);
-			}
-	
-			conn = jndiDS.getConnection();
-			
-			if (conn == null) {
-				log.error("The jndi datasource '" + jndiDatasourceName + "' return a null connection");
-	
-			} else if (conn.isClosed()) {
-				log.error("The jndi datasource '" + jndiDatasourceName + "' returned a closed connection");
-				conn = null;
-			}
-		} else if (jdbcDriver != null && jdbcUrl != null && jdbcUser != null && jdbcPwd != null) {
-			
-			Class.forName(jdbcDriver);
-			conn = DriverManager.getConnection(jdbcUrl, jdbcUser, jdbcPwd);		
-			
-			if (conn == null) {
-				log.error("The jdbc datasource '" + jdbcUrl + "' return a null connection");
-			}
-		}
-
-		
-		return conn;
-	}
 	
 }
