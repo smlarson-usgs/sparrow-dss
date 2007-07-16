cf789d605cf8f99bbb0ae9a8f7e615e51a06f094
Eric Everman <eeverman@usgs.gov>
2007-07-16 16:42:20 +0000

    -minor sys.out change

--- src/gov/usgswim/sparrow/util/SparrowUtil.java
+++ src/gov/usgswim/sparrow/util/SparrowUtil.java
@@ -19,7 +19,6 @@ public class SparrowUtil {
 	}
 
 	public static void print2DArray(Int2D data, String caption, PrintStream stream) {
-	  int[][] values = data.getData();
 		stream.println(caption);
 		
 		if (data.hasHeadings()) {
@@ -28,16 +27,15 @@ public class SparrowUtil {
 			}
 		}
 		
-		for (int i = 0; i < values.length; i++)  {
-		  for (int j = 0; i < values[i].length; i++)  {
-				stream.print("" + values[i][j] + "\t");
+		for (int r = 0; r < data.getRowCount(); r++)  {
+			for (int c = 0; c < data.getColCount(); c++)  {
+				stream.print("" + data.getInt(r, c) + "\t");
 			}
-		  stream.println("");
+			stream.println("");
 		}
 	}
 	
 	public static void print2DArray(Double2D data, String caption, PrintStream stream) {
-	  double[][] values = data.getData();
 		stream.println(caption);
 		
 	  if (data.hasHeadings()) {
@@ -46,9 +44,9 @@ public class SparrowUtil {
 	    }
 	  }
 		
-		for (int i = 0; i < values.length; i++)  {
-			for (int j = 0; j < values[i].length; j++)  {
-				stream.print("" + values[i][j] + "\t");
+		for (int r = 0; r < data.getRowCount(); r++)  {
+			for (int c = 0; c < data.getColCount(); c++)  {
+				stream.print("" + data.getDouble(r, c) + "\t");
 			}
 			stream.println("");
 		}
