09af9ca8cc212dd483cd281418f5ea489425e068
Eric Everman <eeverman@usgs.gov>
2007-10-10 20:43:05 +0000

    Add notes

--- src/gov/usgswim/service/RequestHandler.java
+++ src/gov/usgswim/service/RequestHandler.java
@@ -5,6 +5,9 @@ import java.io.OutputStream;
 /**
  * Implementations are capable of processing a generically defined request bean
  * and returning an output stream.
+ * 
+ * This interface is primarily used for testing.  See HttpRequestHandler for
+ * an interface to use for http requests.
  */
 public interface RequestHandler<T> {
 	public void dispatch(T request, OutputStream out) throws Exception;
