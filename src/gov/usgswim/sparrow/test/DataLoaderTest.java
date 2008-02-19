38ae79fd16893343a8af1fbb25d313224a56bd29
Eric Everman <eeverman@usgs.gov>
2008-02-19 22:46:42 +0000

    Added back original test

--- src/gov/usgswim/sparrow/test/DataLoaderTest.java
+++ src/gov/usgswim/sparrow/test/DataLoaderTest.java
@@ -12,6 +12,7 @@ import gov.usgswim.sparrow.domain.ModelBuilder;
 import gov.usgswim.sparrow.domain.Source;
 import gov.usgswim.sparrow.util.DataLoader;
 
+import gov.usgswim.sparrow.util.JDBCUtil;
 import gov.usgswim.sparrow.util.TabDelimFileUtil;
 
 import java.io.InputStream;
@@ -55,6 +56,25 @@ public class DataLoaderTest extends DataLoaderOfflineTest {
 		
 		conn.close();
 		conn = null;
+	}
+	
+	public void testReadSystemInfo() throws Exception {
+		Data2D jdbcData = JDBCUtil.loadSystemInfo(conn, 22);
+		Data2D dldata = DataLoader.loadSystemInfo(conn, 22);
+		
+		Data2DCompare comp = new Data2DCompare(jdbcData, dldata);
+		
+		
+		for (int c=0; c<comp.getColCount(); c++) {
+			int r = comp.findMaxCompareRow(c);
+			
+			System.out.println("*** max compare in column " + c + " is row " + r);
+			System.out.println("Expected: " + jdbcData.getValue(r, c) + " Actual: " + dldata.getValue(r, c));
+		}
+		
+	  this.assertEquals(0d, comp.findMaxCompareValue());
+		//dumpComparison(jdbcData, dldata, 999999);
+		
 	}
 	
 	/**
