d76a6aabfd47d151de2fc04039a6df67bcfa19dd
Eric Everman <eeverman@usgs.gov>
2009-12-22 20:09:21 +0000

    Add some javadocs and methods to the TestHelper.  Specifically, allowing any type of resource to be loaded based on the name of the test class.
    Former-commit-id: 3952e9506469f299d632518a5d0a235376e5edbc

--- src/test/java/unit/gov/usgswim/sparrow/test/TestHelper.java
+++ src/test/java/unit/gov/usgswim/sparrow/test/TestHelper.java
@@ -135,6 +135,71 @@ public abstract class TestHelper {
 	}
 	
 	/**
+	 * Opens an InputStream to the specified resource fild.
+	 * 
+	 * The file is assumed to have the same name as the passed class,
+	 * but with specified extension.  An additional name suffix may be added to
+	 * allow multiple files for the same class.
+	 * 
+	 * Example 1: If the class is named <code>com.foo.MyClass</code>,
+	 * the file <code>com/foo/MyClass.[passed extension]</code> would be read.
+	 * 
+	 * Example 2: If the class is named <code>com.foo.MyClass</code>, the
+	 * extension "tab", and the name suffix 'file1' is passed,
+	 * the file <code>com/foo/MyClass_file1.tab</code> would be read.  Note the
+	 * automatic addition of the underscore.
+	 * 
+	 * @param forClass The class for which to look for a similar named resource.
+	 * @param fileSuffix A name fragment added to the end of the class name w/ an underscore.
+	 * @param fileExtension The file extension (after the dot, don't include the dot) of the file.
+	 * @return An inputstream from the specified file.
+	 * @throws IOException
+	 */
+	public static InputStream getResource(Class forClass, String fileSuffix, String fileExtension) throws IOException {
+		Properties props = new Properties();
+
+		String basePath = forClass.getName().replace('.', '/');
+		if (fileSuffix != null) {
+			basePath = basePath + "_" + fileSuffix;
+		}
+		basePath = basePath + "." + fileExtension;
+		
+		InputStream is = Thread.currentThread().getContextClassLoader().
+				getResourceAsStream(basePath);
+		
+		return is;
+	}
+	
+	/**
+	 * Loads any type of text resource file as a string.
+	 * 
+	 * The file is assumed to have the same name as the passed class,
+	 * but with specified extension.  An additional name suffix may be added to
+	 * allow multiple files for the same class.
+	 * 
+	 * Example 1: If the class is named <code>com.foo.MyClass</code>,
+	 * the file <code>com/foo/MyClass.[passed extension]</code> would be read.
+	 * 
+	 * Example 2: If the class is named <code>com.foo.MyClass</code>, the
+	 * extension "tab", and the name suffix 'file1' is passed,
+	 * the file <code>com/foo/MyClass_file1.tab</code> would be read.  Note the
+	 * automatic addition of the underscore.
+	 * 
+	 * @param forClass The class for which to look for a similar named resource.
+	 * @param fileSuffix A name fragment added to the end of the class name w/ an underscore.
+	 * @param fileExtension The file extension (after the dot, don't include the dot) of the file.
+	 * @return A string loaded from the specified file.
+	 * @throws IOException
+	 */
+	public static String getFileAsString(Class forClass, String fileSuffix, String fileExtension) throws IOException {
+		
+		InputStream is = getResource(forClass, fileSuffix, fileExtension);
+		
+		String xml = readToString(is);
+		return xml;
+	}
+	
+	/**
 	 * Loads an xml resource file as a string.
 	 * 
 	 * The xml file is assumed to have the same name as the passed class,
@@ -155,16 +220,8 @@ public abstract class TestHelper {
 	 * @throws IOException
 	 */
 	public static String getXmlAsString(Class forClass, String fileSuffix) throws IOException {
-		Properties props = new Properties();
-
-		String basePath = forClass.getName().replace('.', '/');
-		if (fileSuffix != null) {
-			basePath = basePath + "_" + fileSuffix;
-		}
-		basePath = basePath + ".xml";
 		
-		InputStream is = Thread.currentThread().getContextClassLoader().
-				getResourceAsStream(basePath);
+		InputStream is = getResource(forClass, fileSuffix, "xml");
 		
 		String xml = readToString(is);
 		return xml;
