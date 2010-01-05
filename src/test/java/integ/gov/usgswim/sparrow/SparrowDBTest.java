77a717d7acf4317c20cb6dd7e96257deb11c3ad5
Eric Everman <eeverman@usgs.gov>
2010-01-05 19:06:50 +0000

    New base test class that preloads needed system properties to connect to the test db.  Also provides a getConnection method that auto closes the connection at the end of the test.
    Former-commit-id: 8cbd7ab01dd124142d1b88dd9a18ef5a0687cf44

---
+++ src/test/java/integ/gov/usgswim/sparrow/SparrowDBTest.java
@@ -0,0 +1,80 @@
+/**
+ * 
+ */
+package gov.usgswim.sparrow;
+
+import gov.usgswim.sparrow.action.Action;
+import gov.usgswim.sparrow.service.SharedApplication;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.custommonkey.xmlunit.XMLUnit;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+
+/**
+ * A base test class that sets properties needed for a db connection.
+ * @author eeverman
+ *
+ */
+public class SparrowDBTest {
+
+	private static Connection sparrowDBTestConn;
+	
+	protected static Logger log =
+		Logger.getLogger(SparrowDBTest.class); //logging for this class
+	
+	//Application lifecycle listener handles startup / shutdown
+	static LifecycleListener lifecycle = new LifecycleListener();
+	
+	@BeforeClass
+	public static void sparrowDBTestSetUp() throws Exception {
+		
+		//Turns on detailed logging
+		log.setLevel(Level.DEBUG);
+		
+		//Generically turn on logging for Actions
+		log.getLogger(Action.class).setLevel(Level.DEBUG);
+		
+		lifecycle.contextInitialized(null, true);
+		
+		//For comparing xml docs
+		XMLUnit.setIgnoreWhitespace(true);
+		
+		if (System.getProperty("dburl") == null) {
+			setProperties();
+		}
+	}
+
+	@AfterClass
+	public static void sparrowDBTestTearDown() throws Exception {
+		lifecycle.contextDestroyed(null, true);
+		
+		if (sparrowDBTestConn != null) {
+			if (! sparrowDBTestConn.isClosed()) {
+				sparrowDBTestConn.close();
+				sparrowDBTestConn = null;
+			}
+		}
+	}
+	
+	public Connection getConnection() throws SQLException {
+		
+		if (sparrowDBTestConn == null || sparrowDBTestConn.isClosed()) {
+			
+			sparrowDBTestConn = SharedApplication.getInstance().getConnection();
+		}
+		
+		return sparrowDBTestConn;
+	}
+	
+	protected static void setProperties() {
+		System.setProperty("dburl", "jdbc:oracle:thin:@igsarmewdbdev.er.usgs.gov:1521:widev");
+		System.setProperty("dbuser", "sparrow_dss");
+		System.setProperty("dbpass", "***REMOVED***");
+	}
+	
+}
