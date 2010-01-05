89dd0120038cb7d1a386e0a64713178fea3abd89
Eric Everman <eeverman@usgs.gov>
2010-01-05 15:07:48 +0000

    * Add methods to load parameter files w/ substitutions - useful for queries and other text strings * subclass messages now precedes the 'action complete' message in debug logging
    Former-commit-id: adf885805be6220b030e557255066622246a2827

--- src/main/java/gov/usgswim/sparrow/action/Action.java
+++ src/main/java/gov/usgswim/sparrow/action/Action.java
@@ -1,5 +1,9 @@
 package gov.usgswim.sparrow.action;
 
+import java.io.IOException;
+import java.util.Properties;
+
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 
 /**
@@ -90,6 +94,11 @@ public abstract class Action<R extends Object> {
 		if (log.isDebugEnabled()) {
 			long totalTime = System.currentTimeMillis() - startTime;
 			
+			String msg = getPostMessage();
+			if (msg != null) {
+				log.debug(msg + "  (Run Number: " + runNumber + ")");
+			}
+			
 			if (success && error == null) {
 				log.debug("Action completed for " + this.getClass().getName() +
 						".  Total Time: " + totalTime + "ms, Run Number: " +
@@ -108,10 +117,99 @@ public abstract class Action<R extends Object> {
 				}
 			}
 			
-			String msg = getPostMessage();
-			if (msg != null) {
-				log.debug(msg + "  (Run Number: " + runNumber + ")");
-			}
 		}
+	}
+	
+	
+	/**
+	 * Loads the named text chunk from the properties file and inserts the named values passed in params.
+	 *
+	 * This method assumes that the properties are contained in properties file
+	 * in the same package and with the same name as the class.  E.g: pkg.MyClass
+	 * should have a properties file as: pkg/MyClass.properties.
+	 * <br><br>
+	 * params are passed in serial pairs as {"name1", "value1", "name2", "value2"}.
+	 * toString is called on each item, so it is OK to pass in autobox numerics.
+	 * See the DataLoader.properties file for the names of the parameters available
+	 * for the requested query.
+	 *
+	 * @param name	Name of the query in the properties file
+	 * @param params	An array of name and value objects to replace in the query.
+	 * @return
+	 * @throws IOException
+	 */
+	public String getTextWithParamSubstitution(String name, Object... params)
+			throws IOException {
+		return getTextWithParamSubstitution(name, this.getClass(), params);
+	}
+
+	/**
+	 * Loads the named text chunk from the properties file in the same package
+	 * and with the same name as the class. E.g: pkg.MyClass should have a
+	 * properties file as: pkg/MyClass.properties.
+	 * <br><br>
+	 * params are passed in serial pairs as {"name1", "value1", "name2",
+	 * "value2"}. toString is called on each item, so it is OK to pass in
+	 * autobox numerics. See the DataLoader.properties file for the names of the
+	 * parameters available for the requested query.
+	 *
+	 * @param name
+	 *            Name of the query in the properties file
+	 * @param clazz
+	 * @param params
+	 *            An array of name and value objects to replace in the query.
+	 * @return
+	 * @throws IOException
+	 *
+	 * TODO move this to a utils class of some sort
+	 */
+	public static String getTextWithParamSubstitution(String name,
+			Class<?> clazz, Object... params) throws IOException {
+		
+		String query = getText(name, clazz);
+
+		for (int i=0; i<params.length; i+=2) {
+			String n = "$" + params[i].toString() + "$";
+			String v = params[i+1].toString();
+
+			query = StringUtils.replace(query, n, v);
+		}
+
+		return query;
+	}
+
+	/**
+	 * Loads the named text chunk from the properties file.
+	 *
+	 * This method assumes that the properties are contained in properties file
+	 * in the same package and with the same name as the class.  E.g: pkg.MyClass
+	 * should have a properties file as: pkg/MyClass.properties.
+	 *
+	 * @param name
+	 * @return
+	 * @throws IOException
+	 * @
+	 * TODO move this to a utils class of some sort
+	 */
+	public String getText(String name) throws IOException {
+		return getText(name, this.getClass());
+	}
+
+	/**
+	 * Loads the named text chunk from a properties file located in the same
+	 * package and with the same name as the {@code}clazz{@code} parameter
+	 *
+	 * @param name of text chunk
+	 * @param clazz
+	 * @return
+	 * @throws IOException
+	 */
+	public static String getText(String name, Class<?> clazz) throws IOException {
+		Properties props = new Properties();
+
+		String path = clazz.getName().replace('.', '/') + ".properties";
+		props.load(Thread.currentThread().getContextClassLoader().getResourceAsStream(path));
+
+		return props.getProperty(name);
 	}
 }
