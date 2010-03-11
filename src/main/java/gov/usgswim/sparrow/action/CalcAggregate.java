75d8692f2cf54c355b06f384ef79d7e1483d6d81
Eric Everman <eeverman@usgs.gov>
2010-03-11 20:50:18 +0000

    Action to aggregate data by huc.
    Former-commit-id: 4984277d9a4849c4fb5dd7fdf288c18e65335570

---
+++ src/main/java/gov/usgswim/sparrow/action/CalcAggregate.java
@@ -0,0 +1,132 @@
+package gov.usgswim.sparrow.action;
+
+import gov.usgswim.datatable.ColumnDataWritable;
+import gov.usgswim.datatable.DataTable;
+import gov.usgswim.datatable.DataTableWritable;
+import gov.usgswim.datatable.impl.SimpleDataTableWritable;
+import gov.usgswim.datatable.impl.StandardNumberColumnDataWritable;
+import gov.usgswim.sparrow.parser.DataColumn;
+import gov.usgswim.sparrow.service.predict.aggregator.AggregateType;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.TreeMap;
+
+/**
+ * This action creates a ColumnData containing the delivery
+ * fractions to the set of Target reaches.
+ * 
+ * @author eeverman
+ *
+ */
+public class CalcAggregate extends Action<DataColumn> {
+
+	protected DataColumn data;
+	protected DataTable reachHucs;	//as loaded by LoadReachHucs action
+	protected AggregateType aggType;
+	
+	protected String msg = null;	//statefull message for logging
+	
+	/**
+	 * Sets the column of data that is to be aggregated.
+	 * 
+	 * The passed column data must have row ids that are reach ids.
+	 * @param data
+	 */
+	public void setData(DataColumn data) {
+		this.data = data;
+		
+		if (! data.getTable().hasRowIds()) {
+			throw new IllegalArgumentException("The table in the ColumnData must have row ids.");
+		}
+	}
+
+	/**
+	 * Sets the HUC data to use for the aggregation.  The structure of this
+	 * data is defined in the LoadReachHucs action and it must be based on
+	 * the network in use for the results passed for the column data. 
+	 * @param reachHucs
+	 */
+	public void setReachHucs(DataTable reachHucs) {
+		this.reachHucs = reachHucs;
+	}
+
+	/**
+	 * The type of aggregation to do.
+	 * 
+	 * @param aggType
+	 */
+	public void setAggType(AggregateType aggType) {
+		this.aggType = aggType;
+	}
+	
+	@Override
+	protected String getPostMessage() {
+		return msg;
+	}
+	
+	@Override
+	protected DataColumn doAction() throws Exception {
+		
+		//If none type, return the base data.
+		//This could be an error, but its logically consistent.
+		if (aggType.equals(AggregateType.none)) {
+			return data;
+		}
+		
+		
+		DataTable dataTbl = data.getTable();
+		int dataTblCol = data.getColumn();
+		TreeMap<Long, AggregateData> aggDataMap = new TreeMap<Long, AggregateData>();
+		ArrayList<Long> unmatchedRowIds = new ArrayList<Long>();
+		
+		for (int r = 0; r < dataTbl.getRowCount(); r++) {
+			
+			Long rowId = data.getIdForRow(r);
+			Double val = data.getDouble(r);
+			int reachHucRow = reachHucs.getRowForId(rowId);
+			
+			if (reachHucRow > -1) {
+				String hucString = reachHucs.getString(reachHucRow, 0);
+				Long hucId = Long.parseLong(hucString);
+				
+				AggregateData ad = aggDataMap.get(hucId);
+				
+				if (ad == null) {
+					ad = new AggregateData(aggType, hucId);
+					aggDataMap.put(hucId, ad);
+				}
+				
+				ad.add(val);
+				
+			} else {
+				unmatchedRowIds.add(rowId);
+			}
+			
+		}
+		
+		Iterator<AggregateData> it = aggDataMap.values().iterator();
+		ColumnDataWritable column = new StandardNumberColumnDataWritable<Double>().setType(Double.class);
+		column.setUnits(data.getUnits());
+		column.setName(data.getColumnName() +
+				" (Agg'ed to HUC " + data.getTableProperty("huc_level") + ")");
+		
+		DataTableWritable result = new SimpleDataTableWritable();
+		result.addColumn(column);
+		int resultRow = 0;
+		
+		while (it.hasNext()) {
+			AggregateData val = it.next();
+			result.setValue(val.getValue(), resultRow, 0);
+			result.setRowId(val.getAggLevelId(), resultRow);
+			
+			resultRow++;
+		}
+		
+				
+		return new DataColumn(result, 0, null);
+
+	}
+	
+	
+}
---
+++ src/test/java/unit/gov/usgswim/sparrow/action/CalcAggregateTest.java
@@ -0,0 +1,173 @@
+package gov.usgswim.sparrow.action;
+
+import gov.usgswim.datatable.ColumnDataWritable;
+import gov.usgswim.datatable.DataTable;
+import gov.usgswim.datatable.DataTableWritable;
+import gov.usgswim.datatable.impl.SimpleDataTableWritable;
+import gov.usgswim.datatable.impl.StandardNumberColumnDataWritable;
+import gov.usgswim.sparrow.parser.DataColumn;
+import gov.usgswim.sparrow.service.predict.aggregator.AggregateType;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Unit test (offline) of the CalcAggregate Action.
+ * 
+ * TODO:  Nulls are not tested because some of the data2D classes choke on null
+ * values, even though some of our values may actually be nulls.
+ * 
+ * @author eeverman
+ *
+ */
+public class CalcAggregateTest {
+
+	DataTable hucs;
+	DataTable reachValues;
+	DataColumn reachValueCol;
+	
+	@Before
+	public void setup() {
+		ColumnDataWritable hucCol = new StandardNumberColumnDataWritable<Integer>().setType(Integer.class);
+
+		DataTableWritable hucsW = new SimpleDataTableWritable();
+		hucsW.addColumn(hucCol);
+		
+		/////////////////////////
+		// Create the ReachID & Huc associations (normally ceated by the LoadReachHucs Action.
+		/////////////////////////
+		
+		int[] reachHucs = new int[] {
+			102, 2,	/* reach 102 is in huc 2, order shifted to test sort */	
+			101, 1,	
+			103, 3,
+			104, 1, 
+			105, 2,
+			106, 3, 
+			107, 1,
+			108, 2,
+			109, 3
+		};
+		
+		for (int i=0; i < reachHucs.length; i+=2) {
+			hucsW.setValue(reachHucs[i + 1], (i / 2), 0);	//Set the value, which is the HUC ID
+			hucsW.setRowId(reachHucs[i], (i / 2));	//Set the row id, which is the reach ID
+		}
+		
+		hucs = hucsW.toImmutable();
+		
+		
+		/////////////////////////
+		// Create values to aggregate (nominally the prediction values)
+		/////////////////////////
+		Integer[] reachValueArray = new Integer[] {
+				101, 10,	/* reach 101 is in huc 1 */	
+				102, -9,
+				103, 0,
+				104, 11, 
+				105, 3,
+				106, 0, 
+				107, 12,
+				108, -6,
+				109, 0
+			};
+		
+		//Summary by HUC:
+		// 1: 10, 11, 12 (avg 11)
+		// 2: -9, 3, -6 (avg -4)
+		// 3: 0, 0, 0 (avg 0)
+		
+		ColumnDataWritable valueCol = new StandardNumberColumnDataWritable<Double>().setType(Double.class);
+
+		DataTableWritable reachValuesW = new SimpleDataTableWritable();
+		reachValuesW.addColumn(valueCol);
+		
+		for (int i=0; i < reachValueArray.length; i+=2) {
+			reachValuesW.setValue(reachValueArray[i + 1], (i / 2), 0);	//Set the value, which is the HUC ID
+			reachValuesW.setRowId(reachValueArray[i], (i / 2));	//Set the row id, which is the reach ID
+		}
+		
+		reachValues = reachValuesW.toImmutable();
+		reachValueCol = new DataColumn(reachValues, 0, null);
+	}
+	
+	
+	@Test
+	public void average() throws Exception {
+		CalcAggregate action = new CalcAggregate();
+		action.setAggType(AggregateType.avg);
+		action.setData(reachValueCol);
+		action.setReachHucs(hucs);
+		DataColumn result = action.run();
+		
+		//Row 0, HUC 1
+		assertEquals(11d, result.getDouble(0).doubleValue(), .000000000000001d);
+		assertEquals(1L, result.getIdForRow(0).longValue());
+		
+		//Row 1, HUC 2
+		assertEquals(-4d, result.getDouble(1), .000000000000001d);
+		assertEquals(2L, result.getIdForRow(1).longValue());
+		
+		//Row 2, HUC 3
+		assertEquals(0d, result.getDouble(2).doubleValue(), .000000000000001d);
+		assertEquals(3L, result.getIdForRow(2).longValue());
+	}
+	
+	@Test
+	public void min() throws Exception {
+		CalcAggregate action = new CalcAggregate();
+		action.setAggType(AggregateType.min);
+		action.setData(reachValueCol);
+		action.setReachHucs(hucs);
+		DataColumn result = action.run();
+		
+		//asserts
+		assertEquals(10d, result.getDouble(0).doubleValue(), .000000000000001d);
+		assertEquals(-9d, result.getDouble(1), .000000000000001d);
+		assertEquals(0d, result.getDouble(2).doubleValue(), .000000000000001d);
+	}
+	
+	@Test
+	public void max() throws Exception {
+		CalcAggregate action = new CalcAggregate();
+		action.setAggType(AggregateType.max);
+		action.setData(reachValueCol);
+		action.setReachHucs(hucs);
+		DataColumn result = action.run();
+		
+		//asserts
+		assertEquals(12d, result.getDouble(0).doubleValue(), .000000000000001d);
+		assertEquals(3d, result.getDouble(1), .000000000000001d);
+		assertEquals(0d, result.getDouble(2).doubleValue(), .000000000000001d);
+	}
+	
+	@Test
+	public void sum() throws Exception {
+		CalcAggregate action = new CalcAggregate();
+		action.setAggType(AggregateType.sum);
+		action.setData(reachValueCol);
+		action.setReachHucs(hucs);
+		DataColumn result = action.run();
+		
+		//asserts
+		assertEquals(33d, result.getDouble(0).doubleValue(), .000000000000001d);
+		assertEquals(-12d, result.getDouble(1), .000000000000001d);
+		assertEquals(0d, result.getDouble(2).doubleValue(), .000000000000001d);
+	}
+	
+	@Test
+	public void none() throws Exception {
+		CalcAggregate action = new CalcAggregate();
+		action.setAggType(AggregateType.none);
+		action.setData(reachValueCol);
+		action.setReachHucs(hucs);
+		DataColumn result = action.run();
+		
+		//asserts - This should be the reachValues now, not agg data
+		assertEquals(10d, result.getDouble(0).doubleValue(), .000000000000001d);
+		assertEquals(-9d, result.getDouble(1), .000000000000001d);
+		assertEquals(9, result.getRowCount());
+	}
+}
