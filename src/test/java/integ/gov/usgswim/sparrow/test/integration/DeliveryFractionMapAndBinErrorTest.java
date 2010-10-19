57d2b5181b52d4af3967cd818fe7eb612ba12387
Eric Everman <eeverman@usgs.gov>
2010-10-19 16:56:12 +0000

    Fix hardcoded context id issue in test.
    Former-commit-id: ee4528883ffc525369edc5ccd475b7bd75a8837b

--- src/test/java/integ/gov/usgswim/sparrow/test/integration/DeliveryFractionMapAndBinErrorTest.java
+++ src/test/java/integ/gov/usgswim/sparrow/test/integration/DeliveryFractionMapAndBinErrorTest.java
@@ -1,10 +1,10 @@
 package gov.usgswim.sparrow.test.integration;
 
-import static org.junit.Assert.*;
-import gov.usgswim.sparrow.LifecycleListener;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import gov.usgswim.sparrow.MapViewerSparrowDataProvider;
+import gov.usgswim.sparrow.SparrowDBTest;
 import gov.usgswim.sparrow.SparrowUnitTest;
-import gov.usgswim.sparrow.action.Action;
 import gov.usgswim.sparrow.cachefactory.BinningRequest.BIN_TYPE;
 import gov.usgswim.sparrow.service.binning.BinningPipeline;
 import gov.usgswim.sparrow.service.binning.BinningServiceRequest;
@@ -15,11 +15,6 @@ import java.util.Hashtable;
 
 import oracle.mapviewer.share.ext.NSDataSet;
 
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
-import org.custommonkey.xmlunit.XMLUnit;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
@@ -30,68 +25,37 @@ import org.junit.Test;
  * @author eeverman
  * TODO: This should really use a canned project, rather than MRB2
  */
-public class DeliveryFractionMapAndBinErrorTest {
-	protected static Logger log =
-		Logger.getLogger(DeliveryFractionMapAndBinErrorTest.class); //logging for this class
-	
-	static LifecycleListener lifecycle = new LifecycleListener();
-	
-	static final Long MODEL_ID = 50L;
-	
-	
-	@BeforeClass
-	public static void setUp() throws Exception {
-		
-		//Turns on detailed logging
-		log.setLevel(Level.DEBUG);
-		log.getLogger(Action.class).setLevel(Level.DEBUG);
-		
-		lifecycle.contextInitialized(null, true);
-		XMLUnit.setIgnoreWhitespace(true);
-		
-	}
-
-	@AfterClass
-	public static void tearDown() throws Exception {
-		lifecycle.contextDestroyed(null, true);
-	}
+public class DeliveryFractionMapAndBinErrorTest extends SparrowDBTest{
 	
 	@Test
 	public void testFracToNSDataSet() throws Exception {
-		
-		final Integer context_id = 515774381;	//ID of the context we load
 		
 		//Register context from canned file
 		String xmlContextReq = SparrowUnitTest.getXmlAsString(this.getClass(), "context");
 		PredictContextPipeline pipe = new PredictContextPipeline();
 		PredictContextRequest contextReq = pipe.parse(xmlContextReq);
 		String actualResponse = SparrowUnitTest.pipeDispatch(contextReq, pipe);
-		System.out.println(actualResponse);
-		assertTrue(actualResponse.indexOf(context_id.toString()) > -1);
+		
+		Integer contextID = getContextIdFromContext(actualResponse);
+		//System.out.println(actualResponse);
+		//assertTrue(actualResponse.indexOf(context_id.toString()) > -1);
 		
 		//Run a binning request on that same context ID
 		BinningPipeline binPipe = new BinningPipeline();
-		BinningServiceRequest binSvsReq = new BinningServiceRequest(context_id, 5, BIN_TYPE.EQUAL_COUNT);
+		BinningServiceRequest binSvsReq = new BinningServiceRequest(contextID, 5, BIN_TYPE.EQUAL_COUNT);
 		actualResponse = SparrowUnitTest.pipeDispatch(binSvsReq, binPipe);
 		System.out.println("bin response: " + actualResponse);
 		assertTrue(actualResponse.indexOf("<bin>0</bin>") > -1);
 
 		
 		//Build the NSDataset from the data
-		Hashtable props = new Hashtable();
-		props.put(MapViewerSparrowDataProvider.CONTEXT_ID, context_id.toString());
+		Hashtable<String, String> props = new Hashtable<String, String>(2);
+		props.put(MapViewerSparrowDataProvider.CONTEXT_ID, contextID.toString());
 		MapViewerSparrowDataProvider nsProvider = new MapViewerSparrowDataProvider();
 		
 		NSDataSet nsData = nsProvider.buildDataSet(props);
 		assertEquals(8321, nsData.size());
 
-//		BinningRequest binReq = new BinningRequest(context_id, 5, BIN_TYPE.EQUAL_COUNT);
-//		BigDecimal[] bins = SharedApplication.getInstance().getDataBinning(binReq);
-//		
-
-//		System.out.println("bin 1: " + bins[0]);
-//		System.out.println("bin 2: " + bins[1]);
-//		System.out.println("bin 3: " + bins[2]);
 	}
 	
 }
