32b0e7583cd44a443f3c2892a8a1f3681b3ee99f
Eric Everman <eeverman@usgs.gov>
2011-04-19 23:42:47 +0000

    issue SPDSS-602: If you click 'Show Reach Overlay' and have not yet mapped, it fails http://internal.cida.usgs.gov/jira/browse/SPDSS-602
    Former-commit-id: 38aaac5a5c88a60bfefae066b64e84cd979e1d0b

--- src/main/java/gov/usgswim/sparrow/MapViewerSparrowDataProvider.java
+++ src/main/java/gov/usgswim/sparrow/MapViewerSparrowDataProvider.java
@@ -1,8 +1,8 @@
 package gov.usgswim.sparrow;
 
-import gov.usgswim.sparrow.action.NSDataSetBuilder;
-import gov.usgswim.sparrow.cachefactory.NSDataSetFactory;
-import gov.usgswim.sparrow.datatable.SparrowColumnSpecifier;
+import gov.usgswim.sparrow.domain.AdjustmentGroups;
+import gov.usgswim.sparrow.domain.BasicAnalysis;
+import gov.usgswim.sparrow.domain.DataSeriesType;
 import gov.usgswim.sparrow.domain.PredictionContext;
 import gov.usgswim.sparrow.service.SharedApplication;
 
@@ -21,7 +21,17 @@ public class MapViewerSparrowDataProvider implements NSDataProvider {
 
 
 	//Request parameter key constants
+	/**
+	 * Parameter name to specify a context.
+	 */
 	public static final String CONTEXT_ID = "context-id";
+	
+	/** 
+	 * Parameter name to spec a model only.
+	 * If specified, the Total Load of a unmodified model is mapped.
+	 */
+	public static final String MODEL_ID = "model-id";
+	
 
 
 	public MapViewerSparrowDataProvider() {
@@ -86,58 +96,99 @@ public class MapViewerSparrowDataProvider implements NSDataProvider {
 	 */
 	public NSDataSet buildDataSet(Hashtable params) {
 
-		String idString = (String) params.get(CONTEXT_ID);
-		idString = StringUtils.trimToNull(idString);
-
-		if (idString != null) {
+		String contextIdString = (String) params.get(CONTEXT_ID);
+		contextIdString = StringUtils.trimToNull(contextIdString);
+		Integer contextId = null;
+		PredictionContext context;
+		
+		String modelIdString = (String) params.get(MODEL_ID);
+		modelIdString = StringUtils.trimToNull(modelIdString);
+		Long modelId = null;
+		
+		if (contextIdString != null) {
 			log.debug("MapViewerSparrowDataProvider request received w/ context-id = '"
-					+ idString + "'");
-
-			Integer contextId;	//The context identifier to locate the context.
-
+					+ contextIdString + "'");
+			
 			try {
-				contextId = Integer.parseInt(idString);
+				contextId = Integer.parseInt(contextIdString);
 			} catch (NumberFormatException e1) {
 				log.error("MapViewerSparrowDataProvider could not convert passed " +
-				"context-id '" + idString + "' to an integer.", e1);
+				"context-id '" + contextIdString + "' to an integer.", e1);
 				return null;
 			}
-
-			PredictionContext context;
+			
 			try {
 				context = SharedApplication.getInstance().getPredictionContext(contextId);
 			} catch (Exception e) {
 				log.error("Could not fetch PredictionContext from storage.  " +
-						"context-id '" + idString, e);
+						"context-id '" + contextIdString, e);
+				return null;
+			}
+		} else if (modelIdString != null) {
+			
+			log.debug("MapViewerSparrowDataProvider request received w/ model-id = '"
+					+ modelIdString + "'");
+			
+			try {
+				modelId = Long.parseLong(modelIdString);
+			} catch (NumberFormatException e1) {
+				log.error("MapViewerSparrowDataProvider could not convert passed " +
+				"model-id '" + modelIdString + "' to an integer.", e1);
+				return null;
+			}
+			
+			try {
+				context = buildBasicContext(modelId);
+				contextId = context.getId();
+			} catch (Exception e1) {
+				log.error("MapViewerSparrowDataProvider could not create an empty " +
+						"prediction context for model-id '" + modelIdString + "'.", e1);
+						return null;
+			}
+			
+			
+		} else {
+			log.error("MapViewerSparrowDataProvider could not find a model-id" +
+					" or context-id in the request.");
+			return null;
+		}
+		
+
+
+		if (context != null) {
+
+			NSDataSet nsData = null;	// The MapViewer data format for the data
+
+			try {
+				nsData = SharedApplication.getInstance().getNSDataSet(context);
+			} catch (Exception e1) {
+				log.error("MapViewerSparrowDataProvider errored while copying " +
+						"data for context-id '" + contextId, e1);
 				return null;
 			}
 
-			if (context != null) {
+			return nsData;
 
-				NSDataSet nsData = null;	// The MapViewer data format for the data
-
-				try {
-
-					nsData = SharedApplication.getInstance().getNSDataSet(context);
-
-				} catch (Exception e1) {
-					log.error("MapViewerSparrowDataProvider errored while copying " +
-							"data for context-id '" + idString, e1);
-					return null;
-				}
-
-				return nsData;
-
-			}
+		} else {
 			log.info("MapViewerSparrowDataProvider could not find a " +
 					"context for id: " + contextId + ".  It may have expired.");
 			return null;
 		}
-		log.error("MapViewerSparrowDataProvider request received w/o" +
-		" a context-id parameter.");
 
-		return null;
 
+
+	}
+	
+	private PredictionContext buildBasicContext(Long modelId) throws Exception {
+		AdjustmentGroups adjustments = new AdjustmentGroups(modelId);
+		BasicAnalysis analysis = new BasicAnalysis(DataSeriesType.total,
+				null, null, null);
+		PredictionContext context = new PredictionContext(modelId,
+				adjustments, analysis, null, null, null);
+		
+		SharedApplication.getInstance().putPredictionContext(context);
+		
+		return context;
 	}
 
 	/**
