5ecc79fd4f4a828a743bd812b412347607f651a1
Phethala Thongsavanh <thongsav@usgs.gov>
2011-06-03 23:33:59 +0000

    OPEN - issue SPDSS-691: Add a warning when all the reaches fall into one or two bins http://internal.cida.usgs.gov/jira/browse/SPDSS-691
    Former-commit-id: 142d1ad75af6efc7c3a17b6c818ff08d0b81eea0

--- src/main/java/gov/usgswim/sparrow/service/binning/ConfirmBinningService.java
+++ src/main/java/gov/usgswim/sparrow/service/binning/ConfirmBinningService.java
@@ -1,11 +1,11 @@
 package gov.usgswim.sparrow.service.binning;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import gov.usgswim.sparrow.action.VerifyBinningResolution;
 import gov.usgswim.sparrow.action.VerifyInclusiveBinning;
 import gov.usgswim.sparrow.datatable.SparrowColumnSpecifier;
 import gov.usgswim.sparrow.service.AbstractSparrowServlet;
@@ -14,6 +14,10 @@ import gov.usgswim.sparrow.service.ServiceResponseStatus;
 import gov.usgswim.sparrow.service.ServiceResponseWrapper;
 import gov.usgswim.sparrow.service.SharedApplication;
 
+//TODO this service returns two booleans, might want to make it return something more description
+/**
+ * service returns two booleans, first one indicates if all values are in at least one bin, second indicates if all fall into one bin when more than 2 bins are used
+ */
 public class ConfirmBinningService extends AbstractSparrowServlet {
 	
 	/**
@@ -38,6 +42,11 @@ public class ConfirmBinningService extends AbstractSparrowServlet {
 			
 			ServiceResponseWrapper out = new ServiceResponseWrapper(result, Boolean.class, null, ServiceResponseStatus.OK,
 					ServiceResponseOperation.GET);
+			
+			VerifyBinningResolution action2 = new VerifyBinningResolution(data, binHighList, binLowList);
+			Boolean result2 = action2.run();
+			out.addEntity(result2);
+			
 			sendResponse(resp, out);
 		} catch (IOException e) {
 			e.printStackTrace();
---
+++ src/test/java/gov/usgswim/sparrow/action/VerifyBinningResolutionTest.java
@@ -0,0 +1,73 @@
+package gov.usgswim.sparrow.action;
+
+import static org.junit.Assert.*;
+import gov.usgswim.datatable.DataTableWritable;
+import gov.usgswim.sparrow.datatable.SparrowColumnSpecifier;
+import gov.usgswim.sparrow.util.DLUtils;
+import gov.usgswim.sparrow.util.TabDelimFileUtil;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class VerifyBinningResolutionTest {
+	DataTableWritable baseTable;
+	
+	@Before
+	public void doInit() throws Exception {
+		InputStream fileStream =
+			getClass().getResourceAsStream("/gov/usgswim/sparrow/tab_delimit_sample_data.txt");
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(
+				fileStream
+			));
+		
+		baseTable = TabDelimFileUtil.readAsDouble(br,
+				true, DLUtils.DO_NOT_INDEX);
+		
+		//build row IDs matching the row index
+		for (int i=0; i< baseTable.getRowCount(); i++) {
+			baseTable.setRowId(i, i);
+		}
+		
+
+	}
+	
+	@Test
+	public void testBinningResolution() throws Exception{
+		SparrowColumnSpecifier tens = new SparrowColumnSpecifier(baseTable, 0, Integer.valueOf(0));
+		
+		assertEquals(
+				"Test all values in one bin", 
+				Boolean.FALSE, 
+				(new VerifyBinningResolution(
+					tens, 
+					new String[]{"100", "200", "300"},
+					new String[]{"-1", "101", "201"}
+				)).run()
+			);
+		
+		assertEquals(
+				"Test all values in one bin, but only 2 bins", 
+				Boolean.TRUE, 
+				(new VerifyBinningResolution(
+					tens, 
+					new String[]{"100", "200"},
+					new String[]{"-1", "101"}
+				)).run()
+			);
+	
+		assertEquals(
+				"Test values spread accross all 3 bins", 
+				Boolean.TRUE, 
+				(new VerifyBinningResolution(
+					tens, 
+					new String[]{"31", "61", "91"},
+					new String[]{"1", "31", "61"}
+				)).run()
+			);
+	}
+}
