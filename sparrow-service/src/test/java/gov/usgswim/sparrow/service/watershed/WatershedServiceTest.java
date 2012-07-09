61befed7373503bd073a9641ec115978e2fb565a
Eric Everman <eeverman@usgs.gov>
2012-07-09 20:01:14 +0000

    Added tests (more examples) of requesting JSON.
    Former-commit-id: 159e9e9fdda84b4b0c35552d89fc55cd0de19a7c

--- sparrow-service/src/test/java/gov/usgswim/sparrow/service/watershed/WatershedServiceTest.java
+++ sparrow-service/src/test/java/gov/usgswim/sparrow/service/watershed/WatershedServiceTest.java
@@ -26,7 +26,7 @@ public class WatershedServiceTest extends SparrowServiceTestBaseWithDB {
 		WebResponse reportWebResponse = client. sendRequest(webRequest);
 		String actualReportResponse = reportWebResponse.getText();
 		
-		System.out.println(actualReportResponse);
+		//System.out.println(actualReportResponse);
 		
 		String metadataActualColCountStr = getXPathValue("count(//*[local-name()='metadata']//*[local-name()='col'])", actualReportResponse); 
 		String metadataStatedColCountStr = getXPathValue("//*[local-name()='metadata']/@columnCount", actualReportResponse); 
@@ -41,6 +41,23 @@ public class WatershedServiceTest extends SparrowServiceTestBaseWithDB {
 	}
 	
 	@Test
+	public void basicTestOfWatershedListingForAModelAsJSON() throws Exception {
+		
+		//This is the URL we are requesting
+		//Once actually running, the url would be:
+		//[host and port]/sparrow/sp_watershed
+		WebRequest webRequest = new GetMethodWebRequest("http://localhost:8088/sp_watershed?model-id=50&mime-type=json");
+
+		WebResponse reportWebResponse = client. sendRequest(webRequest);
+		String actualReportResponse = reportWebResponse.getText();
+		
+		//System.out.println(actualReportResponse);
+		
+		assertTrue(actualReportResponse.contains("\"@rowCount\": \"16\", \"@columnCount\": \"3\""));
+		assertTrue(actualReportResponse.contains("{ \"r\": [{ \"@id\": \"325\", \"c\": [\"Altamaha River Watershed\", \"Altamaha R\", \"1\"] }"));
+	}
+	
+	@Test
 	public void basicTestOfReachListingForAWatershed() throws Exception {
 		
 		//This is the URL we are requesting
@@ -49,7 +66,7 @@ public class WatershedServiceTest extends SparrowServiceTestBaseWithDB {
 		WebResponse reportWebResponse = client. sendRequest(webRequest);
 		String actualReportResponse = reportWebResponse.getText();
 		
-		System.out.println(actualReportResponse);
+		//System.out.println(actualReportResponse);
 		
 		String metadataActualColCountStr = getXPathValue("count(//*[local-name()='metadata']//*[local-name()='col'])", actualReportResponse); 
 		String metadataStatedColCountStr = getXPathValue("//*[local-name()='metadata']/@columnCount", actualReportResponse); 
@@ -61,6 +78,21 @@ public class WatershedServiceTest extends SparrowServiceTestBaseWithDB {
 		assertEquals("1", actualRowCountStr);
 		assertEquals("1", metadataStatedRowCountStr);
 		
+	}
+	
+	@Test
+	public void basicTestOfReachListingForAWatershedAsJSON() throws Exception {
+		
+		//This is the URL we are requesting
+		WebRequest webRequest = new GetMethodWebRequest("http://localhost:8088/sp_watershed?watershed-id=335&mime-type=json");
+
+		WebResponse reportWebResponse = client. sendRequest(webRequest);
+		String actualReportResponse = reportWebResponse.getText();
+		
+		//System.out.println(actualReportResponse);
+		
+		assertTrue(actualReportResponse.contains("\"@rowCount\": \"1\", \"@columnCount\": \"1\""));
+		assertTrue(actualReportResponse.contains("\"r\": [{ \"@id\": \"18082\", \"c\": [\"TENNESSEE R\"] }]"));
 	}
 	
 }
