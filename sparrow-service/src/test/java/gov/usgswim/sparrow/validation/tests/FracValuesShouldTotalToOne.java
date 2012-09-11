228eec257327022966c04e6d1a49deb8d9a405fa
Eric Everman <eeverman@usgs.gov>
2012-09-11 19:31:49 +0000

    SPDSS-1027
    * FRAC test no longer attempts to add FRACs together for shore reaches
    
    
    Former-commit-id: 14cc0d53e8c8e6ab25f2c8be2f048ed7332d1132

--- sparrow-service/src/test/java/gov/usgswim/sparrow/validation/tests/FracValuesShouldTotalToOne.java
+++ sparrow-service/src/test/java/gov/usgswim/sparrow/validation/tests/FracValuesShouldTotalToOne.java
@@ -82,36 +82,51 @@ public class FracValuesShouldTotalToOne extends SparrowModelValidationBase {
 		for (int row = 0; row < topo.getRowCount(); row++) {
 			Long reachId = predictData.getIdForRow(row);
 			Integer fnode = topo.getInt(row, PredictData.TOPO_FNODE_COL);
-			int[] allReachesAtFromFnode = topo.findAll(PredictData.TOPO_FNODE_COL, fnode);
 			
-			if (allReachesAtFromFnode.length == 0) {
-				this.recordTestException(modelId, null, 
-						"Could not find any reaches with this fnode '" + fnode + "' for reach id " + reachId + " at row " + row);
-				continue;
-			}
-			
-			double fracTotal = 0d;
-			
-			for (int i = 0; i < allReachesAtFromFnode.length; i++) {
-				double thisFrac = topo.getDouble(allReachesAtFromFnode[i], PredictData.TOPO_FRAC_COL);
-				fracTotal+= thisFrac;
-			}
-			
-			if (! comp(1d, fracTotal, allowedFractialVariance)) {
-				Boolean shoreReach = topo.getInt(row, PredictData.TOPO_SHORE_REACH_COL) == 1;
-				Boolean ifTran = topo.getInt(row, PredictData.TOPO_IFTRAN_COL) == 1;
+			Boolean isShoreReach = topo.getInt(row, PredictData.TOPO_SHORE_REACH_COL) == 1;
+			Boolean ifTran = topo.getInt(row, PredictData.TOPO_IFTRAN_COL) == 1;
 				
-				if (allReachesAtFromFnode.length == 1) {
-					this.recordRowError(modelId, reachId, row, "1", fracTotal, "1", "db frac total", shoreReach, ifTran, 
-							"FRAC total != 1 for SINGLE REACH.  FNODE: " + fnode);
-				} else {
-					this.recordRowError(modelId, reachId, row, "1", fracTotal, "1", "db frac total", shoreReach, ifTran, 
-							"FRAC total != 1 for " + allReachesAtFromFnode.length + " reach diversion.  FNODE: " + fnode);
+			
+			if (!isShoreReach) {
+				
+				//This is a regular reach
+				int[] allReachesAtFromFnode = topo.findAll(PredictData.TOPO_FNODE_COL, fnode);
+
+				if (allReachesAtFromFnode.length == 0) {
+					this.recordTestException(modelId, null, 
+							"Could not find any reaches with this fnode '" + fnode + "' for reach id " + reachId + " at row " + row);
+					continue;
 				}
+
+				double fracTotal = 0d;
+
+				for (int i = 0; i < allReachesAtFromFnode.length; i++) {
+					double thisFrac = topo.getDouble(allReachesAtFromFnode[i], PredictData.TOPO_FRAC_COL);
+					fracTotal+= thisFrac;
+				}
+
+				if (! comp(1d, fracTotal, allowedFractialVariance)) {
+
+					if (allReachesAtFromFnode.length == 1) {
+						this.recordRowError(modelId, reachId, row, "1", fracTotal, "1", "db frac total", isShoreReach, ifTran, 
+								"FRAC total != 1 for SINGLE REACH.  FNODE: " + fnode);
+					} else {
+						this.recordRowError(modelId, reachId, row, "1", fracTotal, "1", "db frac total", isShoreReach, ifTran, 
+								"FRAC total != 1 for " + allReachesAtFromFnode.length + " reach diversion.  FNODE: " + fnode);
+					}
+
+				}
+			
+			} else {
+				//This is a shore reach - it doesn't really matter what the FRAC is,
+				//but it probably should be one.
+				double thisFrac = topo.getDouble(row, PredictData.TOPO_FRAC_COL);
 				
+				if (! comp(1d, thisFrac, allowedFractialVariance)) {
+					this.recordRowError(modelId, reachId, row, "1", thisFrac, "1", "db frac total", isShoreReach, ifTran, 
+							"FRAC total != 1 for SINGLE SHORE REACH.  FNODE: " + fnode);
+				}
 			}
-			
-			
 		}
 		
 		
