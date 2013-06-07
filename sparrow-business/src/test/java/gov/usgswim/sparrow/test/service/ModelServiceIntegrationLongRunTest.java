841d19841939b726ea01da800dd48995269ec10f
Eric Everman <eeverman@usgs.gov>
2013-06-07 16:39:15 +0000

    * Fixed this test - broken b/c it had no db connection.  Broken for how long???
    Former-commit-id: 214c45f08dad9be6cc2b8f65ed312dbd77c2dd1b

--- sparrow-business/src/test/java/gov/usgswim/sparrow/test/service/ModelServiceIntegrationLongRunTest.java
+++ sparrow-business/src/test/java/gov/usgswim/sparrow/test/service/ModelServiceIntegrationLongRunTest.java
@@ -1,6 +1,7 @@
 package gov.usgswim.sparrow.test.service;
 
 import gov.usgs.webservices.framework.formatter.XMLPassThroughFormatter;
+import gov.usgswim.sparrow.SparrowTestBaseWithDBandCannedModel50;
 import gov.usgswim.sparrow.service.model.ModelParser;
 import gov.usgswim.sparrow.service.model.ModelRequest;
 import gov.usgswim.sparrow.service.model.ModelService;
@@ -20,92 +21,89 @@ import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
 
-import junit.framework.TestCase;
 
 import org.codehaus.stax2.XMLInputFactory2;
+import org.junit.Test;
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
 /**
  * Unit tests for the {@code ModelService} class.
  */
-public class ModelServiceIntegrationLongRunTest extends TestCase {
+public class ModelServiceIntegrationLongRunTest extends SparrowTestBaseWithDBandCannedModel50 {
 
-    public ModelServiceIntegrationLongRunTest(String sTestName) {
-        super(sTestName);
-    }
+	/**
+	 * Not really much of a test - it just writes the document out to a temp file,
+	 * but it does validate it.
+	 *
+	 * @throws Exception
+	 */
+	@Test
+	public void testBasicMetaRequest() throws Exception {
 
-    /**
-     * Not really much of a test - it just writes the document out to a temp
-     * file, but it does validate it.
-     * 
-     * @throws Exception
-     */
-    public void testBasicMetaRequest() throws Exception {
+		XMLInputFactory xinFact = XMLInputFactory2.newInstance();
+		XMLStreamReader xsr = xinFact.createXMLStreamReader(this.getClass()
+						.getResourceAsStream("/gov/usgswim/sparrow/test/sample/meta_request_1.xml"));
 
-        XMLInputFactory xinFact = XMLInputFactory2.newInstance();
-        XMLStreamReader xsr = xinFact.createXMLStreamReader(this.getClass()
-                .getResourceAsStream("/gov/usgswim/sparrow/test/sample/meta_request_1.xml"));
+		ModelService service = new ModelService();
+		ModelParser parser = new ModelParser();
 
-        ModelService service = new ModelService();
-        ModelParser parser = new ModelParser();
+		File outFile = File.createTempFile("model-service-test", ".xml");
+		FileOutputStream fos = new FileOutputStream(outFile);
 
-        File outFile = File.createTempFile("model-service-test", ".xml");
-        FileOutputStream fos = new FileOutputStream(outFile);
+		ModelRequest req = parser.parse(xsr);
 
-        ModelRequest req = parser.parse(xsr);
+		XMLPassThroughFormatter formatter = new XMLPassThroughFormatter();
+		XMLStreamReader in = service.getXMLStreamReader(req, false);
+		formatter.dispatch(in, fos);
 
-        XMLPassThroughFormatter formatter = new XMLPassThroughFormatter();
-        XMLStreamReader in = service.getXMLStreamReader(req, false);
-        formatter.dispatch(in, fos);
+		fos.close();
+		System.out.println("Result of model request written to: "
+						+ outFile.getAbsolutePath());
 
-        fos.close();
-        System.out.println("Result of model request written to: "
-                + outFile.getAbsolutePath());
+		//assertTrue(validate(outFile.getAbsolutePath())); turned off because can't validate as no xsd provided
+	}
 
-        //assertTrue(validate(outFile.getAbsolutePath())); turned off because can't validate as no xsd provided
-    }
+	public boolean validate(String path) throws ParserConfigurationException,
+					SAXException, IOException {
+		// TODO [IK] Temporarily commented out while jaxp issues are to be fixed
+		// parse an XML document into a DOM tree
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		//dbf.setFeature("http://xml.org/sax/features/namespaces", true);
+		DocumentBuilder parser = dbf.newDocumentBuilder();
 
-    public boolean validate(String path) throws ParserConfigurationException,
-            SAXException, IOException {
-        // TODO [IK] Temporarily commented out while jaxp issues are to be fixed
-        // parse an XML document into a DOM tree
-        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-        //dbf.setFeature("http://xml.org/sax/features/namespaces", true);
-        DocumentBuilder parser = dbf.newDocumentBuilder();
+		Document document = parser.parse(new File(path));
 
-        Document document = parser.parse(new File(path));
+		// create a SchemaFactory capable of understanding WXS schemas
+		SchemaFactory factory = SchemaFactory
+						.newInstance("http://www.w3.org/2001/XMLSchema");
 
-        // create a SchemaFactory capable of understanding WXS schemas
-        SchemaFactory factory = SchemaFactory
-                .newInstance("http://www.w3.org/2001/XMLSchema");
+		// load a WXS schema, represented by a Schema instance
+		Document schemaDoc1 = parser.parse(this.getClass().getResourceAsStream(
+						"/gov/usgswim/sparrow/meta_response.xsd"));
+		System.out.println("Schema 1 document root element: "
+						+ schemaDoc1.getDocumentElement().getNodeName());
+		Source schemaFile1 = new DOMSource(schemaDoc1);
 
-        // load a WXS schema, represented by a Schema instance
-        Document schemaDoc1 = parser.parse(this.getClass().getResourceAsStream(
-                "/gov/usgswim/sparrow/meta_response.xsd"));
-        System.out.println("Schema 1 document root element: "
-                + schemaDoc1.getDocumentElement().getNodeName());
-        Source schemaFile1 = new DOMSource(schemaDoc1);
+		Schema schema = factory.newSchema(new Source[]{schemaFile1});
 
-        Schema schema = factory.newSchema(new Source[] { schemaFile1 });
+		// create a Validator instance, which can be used to validate an
+		// instance document
+		Validator validator = schema.newValidator();
 
-        // create a Validator instance, which can be used to validate an
-        // instance document
-        Validator validator = schema.newValidator();
+		// validate the DOM tree
+		try {
 
-        // validate the DOM tree
-        try {
+			System.out.println("Validation document w/ root element: "
+							+ document.getDocumentElement().getNodeName());
+			validator.validate(new DOMSource(document));
+			return true;
+		} catch (SAXException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+			return false;
+		}
 
-            System.out.println("Validation document w/ root element: "
-                    + document.getDocumentElement().getNodeName());
-            validator.validate(new DOMSource(document));
-            return true;
-        } catch (SAXException e) {
-            System.out.println(e.getMessage());
-            e.printStackTrace();
-            return false;
-        }
-
-        // return false;
-    }
+		// return false;
+	}
 }
