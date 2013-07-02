3a6a5a3f101690075f7474343dc0882e1b67c118
Carl Schroedl <cschroedl@usgs.gov>
2013-07-02 00:38:50 +0000

    Added additional area attribute to the ID window.  Re-annotated a test that was being missed. Un-annotated a method that is not for regular testing.
    Former-commit-id: 562de9d9184fb8b47fda67bd14c361a5e3b57eb8

--- sparrow-business/src/main/java/gov/usgswim/sparrow/service/idbypoint/IDByPointService.java
+++ sparrow-business/src/main/java/gov/usgswim/sparrow/service/idbypoint/IDByPointService.java
@@ -761,13 +761,14 @@ public class IDByPointService implements HttpService<IDByPointRequest> {
 	 * @param req The service request object.
 	 * @param response The service response object.
 	 */
-	private void retrieveAttributes(IDByPointRequest req, IDByPointResponse response)
+	public void retrieveAttributes(IDByPointRequest req, IDByPointResponse response)
 			throws Exception {
+
 
 		DataTable attributes = getAttributeData(response.modelID, response.reachID);
 
 		// TODO [IK] This 4 is hardcoded for now. Have to go back and use SparrowModelProperties to do properly
-		response.basicAttributes = new FilteredDataTable(attributes, new ColumnRangeFilter(0, 13)); // first four columns
+		response.basicAttributes = new FilteredDataTable(attributes, new ColumnRangeFilter(0, 14)); // first four columns
 		response.sparrowAttributes = new FilteredDataTable(attributes, new ColumnRangeFilter(15, attributes.getColumnCount() - 4)); // remaining columns
 
 		StringBuilder basicAttributesSection = toSection(response.basicAttributes, "Basic Attributes", "basic_attrib", DISPLAY_RULES);
