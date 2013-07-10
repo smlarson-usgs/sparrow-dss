2214dde17dade5393a653e51b3e5b7dceb055d0c
Carl Schroedl <cschroedl@usgs.gov>
2013-07-10 15:06:58 +0000

    Restored old test. Updated expected output file to match current anticipated format. Removed useless comments.
    Former-commit-id: 1a51afa3a809c3dc93f35dc9b5e9904fdd33d0b1

--- sparrow-business/src/test/java/gov/usgswim/sparrow/service/idbypoint/IDByPointServiceTest.java
+++ sparrow-business/src/test/java/gov/usgswim/sparrow/service/idbypoint/IDByPointServiceTest.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package gov.usgswim.sparrow.service.idbypoint;
 
 import gov.usgs.cida.datatable.DataTable;
