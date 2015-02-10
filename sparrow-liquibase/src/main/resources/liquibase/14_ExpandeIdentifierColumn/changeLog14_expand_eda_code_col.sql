baa3e82fa13803d80ba862ff7a9f980e27c7efe9
kmschoep <kmschoep@usgs.gov>
2015-02-10 07:51:58 -0600

    expand edaname in model watershed swap table, add slashes to watershed swap script so it can be run in Jenkins.

--- sparrow-liquibase/src/main/resources/liquibase/14_ExpandeIdentifierColumn/changeLog14_expand_eda_code_col.sql
+++ sparrow-liquibase/src/main/resources/liquibase/14_ExpandeIdentifierColumn/changeLog14_expand_eda_code_col.sql
@@ -2,12 +2,12 @@
 
 --This is for the sparrow_dss schema
 
---changeset kmschoep:mod_ancil_temp_eda_code
+--changeset kmschoep:mod_ancil_temp_eda_code2
 alter table temp_ancil modify (edacode varchar2(30 char));
 --rollback alter table temp_ancil modify (edacode varchar2(15 char));
 
 
---changeset kmschoep:mod_model_reach_attrib_eda_code
+--changeset kmschoep:mod_model_reach_attrib_eda_code2
 alter table model_reach_attrib modify (edacode varchar2(30 char));
 --rollback alter table model_reach_attrib modify (edacode varchar2(15 char));
 
@@ -20,3 +20,12 @@ alter table temp_ancil modify (edaname varchar2(85 char));
 --changeset kmschoep:mod_model_reach_attrib_eda_name
 alter table model_reach_attrib modify (edaname varchar2(85 char));
 --rollback alter table model_reach_attrib modify (edaname varchar2(60 char));
+
+
+--changeset kmschoep:mod_model_reach_attrib_swap_eda_code
+alter table model_reach_attrib_swap modify (edacode varchar2(30 char));
+--rollback alter table model_reach_attrib_swap modify (edacode varchar2(10 char));
+
+--changeset kmschoep:mod_model_reach_attrib_swap_eda_name
+alter table model_reach_attrib_swap modify (edaname varchar2(85 char));
+--rollback alter table model_reach_attrib_swap modify (edaname varchar2(30 char));
