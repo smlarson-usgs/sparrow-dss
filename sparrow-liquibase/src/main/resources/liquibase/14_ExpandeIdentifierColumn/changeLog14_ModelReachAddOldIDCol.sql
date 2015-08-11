26de7cc4a21c19b1e63cdfa807f86f41bcc6071d
kmschoep <kmschoep@usgs.gov>
2015-08-11 09:09:09 -0500

    add entry to handle the model_reach_swap table better.

--- sparrow-liquibase/src/main/resources/liquibase/14_ExpandeIdentifierColumn/changeLog14_ModelReachAddOldIDCol.sql
+++ sparrow-liquibase/src/main/resources/liquibase/14_ExpandeIdentifierColumn/changeLog14_ModelReachAddOldIDCol.sql
@@ -28,6 +28,14 @@ ALTER TABLE MODEL_REACH_SWAP ADD (OLD_IDENTIFIER number(10,0));
 --rollback update model_reach_swap set identifier = old_identifier;
 --rollback alter table MODEL_REACH_SWAP drop COLUMN OLD_IDENTIFIER;
 
+--changeset kmschoep:copyIDENTIFIERtoOLD_IDENTIFIERswap
+--preconditions onFail:HALT onError:HALT
+UPDATE MODEL_REACH_SWAP
+SET OLD_IDENTIFIER = IDENTIFIER
+WHERE
+      identifier is not null;
+--rollback select null from dual;  
+
 
 --changeset kmschoep:modIDENTIFIER10digitsSwap
 --preconditions onFail:HALT onError:HALT
