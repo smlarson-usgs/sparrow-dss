3aa5e3a49e2abcaa7e86a11cad58f13a40bfa385
kmschoep <kmschoep@usgs.gov>
2015-08-21 14:12:53 -0500

    map column in text file to total_contrib_area, add script for updating temp_ancil with back-loaded huc8.

--- sparrow-etl/populateTempAncilHuc.sql
+++ sparrow-etl/populateTempAncilHuc.sql
@@ -5,10 +5,10 @@ whenever sqlerror exit failure rollback;
 whenever oserror exit failure rollback;
 select 'start time: ' || systimestamp from dual;
 
-update temp_ancil
-   set huc8 = (select huc8_char
-                 from temp_huc_list
-                where temp_ancil.local_id = temp_huc_list.comid);
+merge into temp_ancil ta
+using (select comid, huc8_char from temp_huc_list) hl
+on (ta.mrb_id = hl.comid)
+when matched then update set ta.huc8 = hl.huc8_char;
 
 commit;
 
